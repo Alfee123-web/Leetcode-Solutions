a785432c1c732f07acc92e1ad5fb5e091e96ebe5
Alfee Khan <alfeekhan20@gmail.com>
2026-06-24 19:34:38 +0530

    Time: 0 ms (100%), Space: 19.7 MB (32.9%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>mp;
         const int offset = 1000;
-        const int size = 2000;
+        // shift upto 1000
         for(int i = 0; i< nums.size();i++){
             int comp = target - nums[i];
             if( comp <= 1000 && comp >= -1000 &&mp.find(comp + offset) != mp.end()){
