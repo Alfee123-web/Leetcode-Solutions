843914d4f116918f60147972596d6aca749a9c40
Alfee Khan <alfeekhan20@gmail.com>
2026-06-30 11:08:47 +0530

    Time: 50 ms (47.57%), Space: 31.1 MB (21.97%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //sorting => 2 pointer approach
-        vector<vector<int>> ans;
-        sort(nums.begin() , nums.end());
-        for(int i = 0; i< nums.size(); i++){
+        int n = nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            int j = i+1;
-            int k = nums.size()-1;
-            while(j < k){
-                int s = nums[i] + nums[j] + nums[k];
-                if(s < 0){
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+               int s = nums[i] + nums[j] + nums[k];
+                if (s < 0) {
                     j++;
-                }else if(s > 0){
+
+                } else if(s> 0) {
                     k--;
-                }else{
-                    vector<int> temp = {nums[i] , nums[j], nums[k]};
+                } else {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
                     ans.push_back(temp);
+
                     j++;
                     k--;
-                    while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
-                    //out pf bound check
+
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
             }
         }
-  return ans;
+        return ans;
     }
 };
