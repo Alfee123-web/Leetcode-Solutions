e953b0a0405fed1ec94d6b65c04fd7b11b36a363
Alfee Khan <alfeekhan20@gmail.com>
2026-07-08 12:37:43 +0530

    Time: 0 ms (100%), Space: 15.7 MB (72.86%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,29 +11,29 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* dummy = new ListNode(0 , head);
-        ListNode* prev = dummy;
-      
-       while(head != NULL){
-        if(head->next != NULL && head->val == head->next->val){
-            while(head->next != NULL && head->val == head->next->val){
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-        }
-        ListNode* duptail = head;
-        prev->next = head->next;
-        head = head->next;
-        delete duptail;
-        }
-        else{
-            prev = prev->next;
-            head= head->next;
-        }
-       }
-       ListNode* newHead = dummy->next;
-       delete dummy;
-      return newHead;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+
+        while (head != NULL) {
+            if (head->next != NULL && head->val == head->next->val) {
+                while (head->next != NULL && head->val == head->next->val) {
+                    ListNode* temp = head;head = head->next;
+                    delete temp;
+                }
         
+            ListNode* deltail = head;
+            prev->next = head->next;
+            head = head->next;
+            delete deltail;
+            }
+            else {
+                head = head->next;
+                prev = prev->next;
+            }
+        }
+
+        return dummy.next;
     }
+
 };
