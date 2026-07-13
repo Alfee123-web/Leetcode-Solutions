2c47aa16e35a720f5cfe1c5e0697ad45360b71fa
Alfee Khan <alfeekhan20@gmail.com>
2026-07-13 11:40:30 +0530

    Time: 3 ms (48.12%), Space: 77 MB (49.3%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -9,35 +9,39 @@
  * };
  */
 class Solution {
-    ListNode *reverseList(ListNode* head){
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-        while(curr){
-            ListNode *next = curr->next;
+    ListNode* reverse(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while (curr) {
+            ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = next;     
+            curr = next;
         }
         return prev;
     }
+
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1 = reverseList(l1);
-        l2 = reverseList(l2);
-    ListNode dummy(0);
-    ListNode *ptr = &dummy;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        ListNode dummy(0);
+        ListNode* ptr = &dummy;
         int carry = 0;
-         while(l1 != NULL || l2!= NULL || carry != 0){
-              int s = carry;
-              if(l1){
-                s += l1->val ; l1 = l1->next;
-              }if(l2){
-                s += l2->val ; l2 = l2->next;
-              }
-              carry = s /10;
-               ptr->next = new ListNode(s%10);
-               ptr = ptr->next;
-         }
-         return reverseList(dummy.next);
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int s = carry;
+            if (l1) {
+                s += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                s += l2->val;
+                l2 = l2->next;
+            }
+            carry = s / 10;
+            ptr->next = new ListNode(s % 10);
+            ptr = ptr->next;
+        }
+        return reverse(dummy.next);
     }
 };
