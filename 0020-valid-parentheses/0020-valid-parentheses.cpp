1c3b25ae290abe0a90bebf0d58bb4beac0291a1c
Alfee Khan <alfeekhan20@gmail.com>
2026-07-19 13:01:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for (int i = 0; i < s.size(); i++) {
+        stack<char>t;
+        for(int i =0 ;i < s.size();i++){
             char ch = s[i];
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            } else {
-                if (st.empty()) {
+            //opening
+            if(ch == '(' || ch == '[' || ch == '{'){
+                t.push(ch);
+            }else{
+                if(t.empty()){
                     return false;
                 }
-                int top = st.top();
-                //matching condition
-                if ((top == '(' && ch == ')') ||
-                    (top == '{' && ch == '}') || (top == '[' && ch == ']')) {
-                    st.pop();
-                } else {
-                    return false;
-                }
+            //matching
+            int top = t.top();
+            if((top == '{' && ch == '}') 
+            || (top == '[' && ch == ']') 
+            || (top == '(' && ch == ')') ){
+                t.pop();
+            }else{
+                return false;
+            }
             }
         }
-        return st.empty();
+         return t.empty();
     }
 };
