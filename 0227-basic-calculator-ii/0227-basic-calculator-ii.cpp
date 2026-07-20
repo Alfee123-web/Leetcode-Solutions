3f78dc98b4da81965a9751618ce8c2cde9f4fd49
Alfee Khan <alfeekhan20@gmail.com>
2026-07-20 21:00:23 +0530

    Time: 8 ms (44.68%), Space: 15 MB (71.98%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int currN = 0;
+        char sign = '+';
+        //prev op we saw
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+
+            if (isdigit(ch)) {
+                currN = currN * 10 + (ch - '0');
+            }
+
+            if ((!isdigit(ch) && ch != ' ') || i == s.length() - 1) {
+
+                if (sign == '+')
+                    st.push(currN);
+                else if (sign == '-')
+                    st.push(-currN);
+                else if (sign == '*') {
+                    int top = st.top();
+                    st.pop();
+                    st.push(top * currN);
+                } else if (sign == '/') {
+                    int top = st.top();
+                    st.pop();
+                    st.push(top / currN);
+                }
+
+                sign = ch;
+                currN = 0;
+            }
+        }
+
+        int ans = 0;
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        return ans; // 3 2
+    }
+};
