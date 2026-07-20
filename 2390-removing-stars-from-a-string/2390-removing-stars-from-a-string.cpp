9a03fbb894cfc9ff29297101c15fab98cbda480e
Alfee Khan <alfeekhan20@gmail.com>
2026-07-20 19:52:59 +0530

    Time: 20 ms (48.26%), Space: 29.8 MB (24.26%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     string removeStars(string s) {
-        std::stack<char>st;
-        for(char ch : s ){
-            if(ch == '*'){
-                if(!st.empty()){
-  st.pop();
-                }
-            }else{
-                st.push(ch);
-            }
-        }
-           string ans ="";
-           while(!st.empty()){
-           ans +=  st.top();
-           st.pop();
-           }
+        std::stack<char> st;
+        for (char ch : s) {
+            if (ch == '*') {
 
-        reverse(ans.begin() , ans.end());
-        return ans;
+                st.pop();
+            }
+        
+        else {
+            st.push(ch);
+        }
     }
-};
+    string ans = "";
+    while (!st.empty()) {
+        ans += st.top();
+        st.pop();
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+}
+;
