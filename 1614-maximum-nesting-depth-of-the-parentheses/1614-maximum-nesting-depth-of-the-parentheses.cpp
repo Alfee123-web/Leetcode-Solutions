ad3a08aa8d61c93b65f65e775ba8f4828027ab8a
Alfee Khan <alfeekhan20@gmail.com>
2026-07-23 11:30:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.48%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int depth = 0; 
+        // stack<int>st;
+        int depth = 0;
         int maxD = 0;
-        for(int i = 0; i< s.size(); i++){
-            if(s[i] == '('){
+        for(int i =0 ; i <s.length();i++){
+            char ch = s[i];
+            if(ch == '('){
+                // st.push(ch);
                 depth++;
-                maxD = max(maxD , depth);
-            }else if (s[i]== ')'){
+                maxD = max(depth , maxD);
+            }else{
+            if( ch == ')')
+                // st.pop();
                 depth--;
-            }
         }
-        return maxD;
+        }
+return maxD;
     }
 };
