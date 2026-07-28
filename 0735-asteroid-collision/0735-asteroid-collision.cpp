5e7a1d0403746cbd7d0b56646848a596e1d38be8
Alfee Khan <alfeekhan20@gmail.com>
2026-07-28 12:42:26 +0530

    Time: 0 ms (100%), Space: 21.6 MB (79.68%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int>ans;
-        for(int i : asteroids){
-            if(i > 0){
+        vector<int> ans;
+        for (int i : asteroids) {
+            if (i > 0) {
                 ans.push_back(i);
-            }else{
-                while(!ans.empty() && ans.back() > 0 && ans.back() < abs(i)){
-                    //positive small ones pop 
+            } else if (i < 0) {
+                while (!ans.empty() && ans.back() > 0 && ans.back() < abs(i)) {
                     ans.pop_back();
                 }
                 if(ans.empty() || ans.back() < 0){
                     ans.push_back(i);
-                }else if(ans.back() == abs(i)){
-                    ans.pop_back();
-                }else{
-
                 }
+                else if(ans.back() == abs(i)) {
+                    ans.pop_back();
+                }
+               
             }
         }
-        return ans;
-    }
-};
+
+    return ans;
+}
+}
+;
