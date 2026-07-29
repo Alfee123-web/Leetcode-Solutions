21f249225aceec9b516f4fb153ab177878054e69
Alfee Khan <alfeekhan20@gmail.com>
2026-07-29 12:05:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (6.07%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> count(26, 0);
-        vector<bool> instack(26, false);
-        for (char ch : s) {
-            count[ch - 'a']++;
+        vector<int>count(26 , 0);
+        vector<bool>instack(26 , false);
+        //unique
+        for(char ch : s){
+            count[ch -'a']++;
         }
-        std::stack<char> st;
-
-        for (char ch : s) {
-            count[ch - 'a']--;
-
-            if (instack[ch - 'a'])
-                continue;
-            // order maintain
-            while (!st.empty() && st.top() > ch && count[st.top() - 'a'] > 0) {
-                instack[st.top() - 'a'] = false;
-                st.pop();
-            }
-            // insert curr element
-            st.push(ch);
-            instack[ch - 'a'] = true;
+        stack<int>st;
+        for(char ch : s){
+            count[ch-'a']--;
+            //lexographical
+       // Prevents Duplicate Insertio
+        if(instack[ch-'a']){
+            continue;
+        }
+      //   count[st.top()-'a'] exist karta aage string me ya nahi 
+        while(!st.empty() && st.top() > ch &&  count[st.top()-'a'] > 0){
+              instack[st.top()-'a']=false;
+            st.pop();
+          
+        }
+        st.push(ch);
+        instack[ch-'a'] = true;
         }
         string ans = "";
-        while (!st.empty()) {
+        while(!st.empty()){
             ans += st.top();
             st.pop();
         }
         reverse(ans.begin(), ans.end());
-
         return ans;
     }
 };
