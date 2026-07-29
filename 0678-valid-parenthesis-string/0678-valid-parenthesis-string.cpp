06319d3e36417840472d8c6710f5a589f449eca9
Alfee Khan <alfeekhan20@gmail.com>
2026-07-29 12:27:45 +0530

    Time: 1 ms (14.67%), Space: 8.4 MB (21.32%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -3,30 +3,30 @@ public:
     bool checkValidString(string s) {
         stack<int>o;
         stack<int>st;
-        for(int i = 0; i< s.length(); i++){
-            char ch = s[i];
+        for(int i = 0; i< s.length();i++){
+             char ch = s[i];
             if(ch == '('){
                 o.push(i);
-            }else if(ch == '*'){
-                st.push(i);
-            }else{
-                if(!o.empty()){
+            }else if (ch == '*'){
+               st.push(i);
+               }else{
+
+               if(!o.empty()){
                     o.pop();
                 }else if(!st.empty()){
-                    st.pop();
+                  st.pop();
                 }else{
                     return false;
                 }
             }
         }
-            while(!o.empty() && !st.empty()){
-                 if(o.top() > st.top()){
- return false;
-                 }
-               o.pop();
-               st.pop();
+        while(!o.empty() && !st.empty()){
+            if(o.top() > st.top()){
+                return false;
             }
-            return o.empty();
-    
+            o.pop();
+            st.pop();
+        }
+        return o.empty();
     }
 };
