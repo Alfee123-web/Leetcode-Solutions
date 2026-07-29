428e508f83fe1afd54732b658308a46a409ca413
Alfee Khan <alfeekhan20@gmail.com>
2026-07-29 11:34:36 +0530

    Time: 1 ms (19.95%), Space: 13 MB (18.57%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     int calPoints(vector<string>& operations) {
         stack<int>st;
-        for(const string& ch : operations){
-            if(ch == "C"){
-                st.pop();
-            }else if(ch == "D"){
-                int top = st.top();
-                st.push(2 * top);
-            }else if(ch == "+"){
-                int t1 = st.top();
-                st.pop();
-                int t2 = st.top();
-           
-                int sum = t1 + t2;
+        for(int i = 0; i< operations.size();i++){
+            
+            if(operations[i]== "+"){
+                int t1 = st.top(); st.pop();
+                int t2 = st.top(); 
                 st.push(t1);
-                st.push(sum);
+                st.push(t1 + t2);
+            }else if(operations[i] == "C"){
+                st.pop();
+            }else if(operations[i] == "D"){
+                int t1 = st.top(); 
+                st.push(t1 * 2);
             }else{
-                st.push(stoi(ch));
+                st.push(stoi(operations[i]));
             }
         }
-        int Tsum = 0;
+        int ans = 0;
         while(!st.empty()){
-            int top = st.top();
-            Tsum += st.top();
+            ans += st.top();
             st.pop();
         }
-        return Tsum;
+        return ans;
     }
 };
