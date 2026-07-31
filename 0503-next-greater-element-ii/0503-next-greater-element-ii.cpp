491ce16198e51660a5fcdd6bec717168053359f7
Alfee Khan <alfeekhan20@gmail.com>
2026-07-31 11:51:20 +0530

    Time: 3 ms (74.33%), Space: 28.2 MB (92.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+
         int n = nums.size();
-        vector<int>ans(n , -1);
-        stack<int>st;
-        //2*n = circular
-        //i%n = out of bound case 
-        
-        for(int i = 0; i < 2*n;i++){
-            while(!st.empty() && nums[i % n] > nums[st.top()]){
+        vector<int> ans(n, -1);
+        for (int i = 0; i < 2 * n; i++) {
+            while (!st.empty() && nums[i % n] > nums[st.top()]) {
                 int t = st.top();
                 st.pop();
-                ans[t] = nums[i%n]; 
+                ans[t] = nums[i % n];
             }
-            if(i < n){
+            if (i < n) {
                 st.push(i);
             }
         }
