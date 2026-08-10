a8716deeb7616c048d5ccd9662b779b330d82202
Alfee Khan <alfeekhan20@gmail.com>
2026-08-10 12:15:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        vector<int>freq(26,0);
+        for(char ch : s){
+            freq[ch-'a']++;
+        }
+        for(char ch : t){
+            if(freq[ch-'a'] == 0 ) return false;
+             freq[ch-'a']--;
+         
+        }
+        return true;
+    }
+};
