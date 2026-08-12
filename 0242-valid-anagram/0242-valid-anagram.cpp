553105e05a23ae935d698fb0619fe5d0de2087e3
Alfee Khan <alfeekhan20@gmail.com>
2026-08-12 12:25:41 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        vector<int>freq(26,0);
-        for(char ch : s){
-            freq[ch-'a']++;
+        vector<int>f(26,0);
+        for(int i = 0 ; i< s.length();i++){
+               f[s[i]-'a']++;
         }
-        for(char ch : t){
-            if(freq[ch-'a'] == 0 ) return false;
-             freq[ch-'a']--;
-         
+        for(int i = 0 ; i< t.length();i++){
+            if(f[t[i]-'a'] == 0){
+                return false;
+            }
+                  f[t[i]-'a']--;
+          
         }
         return true;
     }
