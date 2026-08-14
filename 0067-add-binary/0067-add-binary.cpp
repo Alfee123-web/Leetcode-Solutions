9d27c9f57eb75921d04a7da615f75586ffe37fad
Alfee Khan <alfeekhan20@gmail.com>
2026-08-14 19:38:55 +0530

    Time: 2 ms (22.42%), Space: 8.9 MB (81.06%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -3,22 +3,23 @@ public:
     string addBinary(string a, string b) {
         int i = a.length()-1;
         int j = b.length()-1;
+        string ans= "";
         int c = 0;
-        string ans = "";
         while(i >= 0 || j >= 0 || c > 0){
             int s = c;
             if(i >= 0){
                 s += a[i] - '0';
                 i--;
             }
-               if(j >= 0){
+            if(j >= 0){
                 s += b[j] - '0';
                 j--;
             }
-            ans += (s % 2) + '0';
+            ans += ( s % 2) + '0';
             c = s/2;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+            reverse(ans.begin(),ans.end());
+            return ans;
+        
     }
 };
