93aca9e2a8aae9250ce6e88298b089a5925ddfa5
Alfee Khan <alfeekhan20@gmail.com>
2026-08-28 11:53:43 +0530

    Time: 5 ms (98.78%), Space: 80.5 MB (60.07%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
-void merge(vector<int>&nums , int s , int m , int e){
-    vector<int>temp;
-    int i = s;
-    int j = m+1;
-    while(i <= m && j <= e){
-             if(nums[i] < nums[j]){
-                temp.push_back(nums[i]);
-                i++;             
-    }else{
-        temp.push_back(nums[j]);
-        j++;
-    }
-}
-while(i <= m){
-       temp.push_back(nums[i]);
-       i++;
-}
-while(j <= e){
-       temp.push_back(nums[j]);
-       j++;
-}
-for(int idx = 0;idx < temp.size();idx++){
-    nums[s + idx] = temp[idx];
-}
-}
-  void mergeSort(vector<int>&nums , int s , int e){
-    if(s>=e){
-        return;
-    }
-    int m = s + (e-s)/2;
-    mergeSort(nums , s , m);
-    mergeSort(nums , m+1,e);
-
-    merge(nums , s, m , e);
-  }
     vector<int> sortArray(vector<int>& nums) {
-        if(nums.empty()) return nums;
-        mergeSort(nums , 0 , nums.size()-1);
+        //COUNT SORT
+        int minV = -50000;
+        int maxV = 50000;
+        int range = maxV - minV + 1;
+        vector<int>freq(range , 0);
+        for(int num : nums){
+                freq[num - minV]++;
+        }
+        int idx =0;
+        for(int i = 0; i < range ; i++){
+            while(freq[i] > 0){
+                nums[idx++] = i + minV;
+                freq[i]--;
+            }
+        }
         return nums;
+
     }
 };
