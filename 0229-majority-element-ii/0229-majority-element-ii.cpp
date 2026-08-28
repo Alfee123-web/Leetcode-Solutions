94150fa95a352e64f4beb1eae0b8654e7f7d0975
Alfee Khan <alfeekhan20@gmail.com>
2026-08-28 12:08:49 +0530

    Time: 4 ms (45.58%), Space: 26.5 MB (16.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int c= 1;
-        vector<int>ans;
-        sort(nums.begin(),nums.end());
-        for(int i = 1 ;i <= n ;i ++){
-           if(i < n && nums[i] == nums[i-1]){
-            c++;
-           }else{
-            if(c > n/3){
-                ans.push_back(nums[i-1]);
-            }
-            c= 1;
-           }
+         vector<int>ans;
+        unordered_map<int ,int>freq;
+
+        for(int num : nums){
+              freq[num]++;//1 2
         }
+       for(auto const& [key,count]:freq){
+        if(count > n/3){
+               ans.push_back(key);
+        }
+       }
         return ans;
     }
 };
