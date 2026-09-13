d7e7745b80ce7d6e9db36955076be200ddccaa88
Alfee Khan <alfeekhan20@gmail.com>
2026-09-13 23:01:52 +0530

    Time: 146 ms (20.85%), Space: 16.1 MB (21.97%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        vector<pair<int, int>> list1, list2;
+        
+        // Collect coordinates of all 1s
+        for (int r = 0; r < n; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (img1[r][c] == 1) list1.push_back({r, c});
+                if (img2[r][c] == 1) list2.push_back({r, c});
+            }
+        }
+        
+        // Map to store frequency of each translation vector
+        map<pair<int, int>, int> translationCount;
+        int maxOverlap = 0;
+        
+        for (auto& p1 : list1) {
+            for (auto& p2 : list2) {
+                int dr = p2.first - p1.first;
+                int dc = p2.second - p1.second;
+                translationCount[{dr, dc}]++;
+                maxOverlap = max(maxOverlap, translationCount[{dr, dc}]);
+            }
+        }
+        
+        return maxOverlap;
+    }
+};
